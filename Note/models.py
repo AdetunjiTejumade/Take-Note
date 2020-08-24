a2c2ba73ee5224a1dba24ed18b291e6b893a39e4
Adetunji Tejumade <tejumadeadetunji@gmail.com>
2020-08-24 09:39:31 +0100

    update virtualenv

--- Note/models.py
+++ Note/models.py
@@ -2,10 +2,11 @@ from django.db import models
 
 from django.urls import reverse
 # Create your models here.
+
+
 class Note(models.Model):
     body = models.TextField()
     date_added = models.DateTimeField(auto_now="True")
-
 
     def __str__(self):
         return self.body[:50]
